4818df25de47d9b4442967b6d362b1667d4106b7
Andrzej Krok <andrzejkrok111@gmail.com>
2023-03-08 20:31:50 +0100

    zadanie dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -3,18 +3,18 @@ string sex = "kobieta";
 var age = 33;
 
 if (sex == "kobieta" && age < 30)
-{
-    Console.WriteLine("Kobieta poniżej 30 lat");
-}
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
 else if (name == "Ewa" && age == 33)
-{
-    Console.WriteLine("Ewa lat 33");
-}
-else if (sex == "mężczyzna" && age < 18)
-{
-    Console.WriteLine("Nieletni mężczyzna");
-}
-else
-{
-    Console.WriteLine("Ktoś inny");
-}
+    {
+        Console.WriteLine("Ewa lat 33");
+    }
+    else if (sex == "mężczyzna" && age < 18)
+    {
+        Console.WriteLine("Nieletni mężczyzna");
+    }
+    else
+    {
+        Console.WriteLine("Ktoś inny");
+    }
