ce28ec5cbc46db38a18bea0cb6b9c54676e96c1d
Andrzej Krok <andrzejkrok111@gmail.com>
2023-05-03 22:05:57 +0200

    zadanie dzień 10

--- ChallengeApp/ChallengeApp/Employee.cs
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -10,10 +10,54 @@
             this.Name = name;
             this.Surname = surname;
         }
-
         public string Name { get; private set; }
         public string Surname { get; private set; }
+        public void AddGrade(float grade)
+        {
 
+            int valueInInt = (int)grade;
+            float f = valueInInt;
+
+            if (grade >= 0 && grade <= 100)
+            {
+                this.grades.Add(grade);
+            }
+            else
+            {
+                Console.WriteLine("invalid grade value");
+            }
+        }
+
+
+        public void AddGrade(string grade)
+        {
+            if (float.TryParse(grade, out float result))
+            {
+                this.AddGrade(result);
+            }
+            else
+            {
+                Console.WriteLine("String is not float");
+            }
+        }
+
+        public void AddGrade(double grade)
+        {
+            float gradeAsFloat = (float)grade;
+            this.AddGrade(gradeAsFloat);
+        }
+
+        public void AddGrade(long grade)
+        {
+            float gradeAsFloat = (float)grade;
+            this.AddGrade(gradeAsFloat);
+        }
+
+        public void AddGrade(int grade)
+        {
+            float gradeAsFloat = grade;
+            this.AddGrade(gradeAsFloat);
+        }
         public Statistics GetStatistics()
         {
             var statistics = new Statistics();
@@ -31,9 +75,6 @@
             statistics.Average /= this.grades.Count;
             return statistics;
         }
-        public void AddGrade(float grade)
-        {
-            this.grades.Add(grade);
-        }
+
     }
 }
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,16 +1,16 @@
 ﻿using ChallengeApp;
 
 var employee = new Employee("Kuba", "Zasada");
-employee.AddGrade(2);
-employee.AddGrade(7);
-employee.AddGrade(6);
+employee.AddGrade("Kuba");
+employee.AddGrade("2000");
+employee.AddGrade(6000111215512151200);
+employee.AddGrade(6.5);
+employee.AddGrade(200);
+employee.AddGrade(3.5); 
+employee.AddGrade(20);
+employee.AddGrade(60);
+employee.AddGrade(60);
 var statistics = employee.GetStatistics();
 Console.WriteLine($"Average: {statistics.Average:N2}");
 Console.WriteLine($"Min: {statistics.Min}");
 Console.WriteLine($"Max: {statistics.Max}");
-
-SetSth(out statistics);
-void SetSth(out Statistics statistics)
-{
-    statistics = new Statistics();
-}
